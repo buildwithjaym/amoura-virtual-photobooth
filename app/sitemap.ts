35a9cc5f271edd282c7d7d92974b7842adfc8a41
buildwithjaym <jaymmaruji@gmail.com>
2026-05-02 17:11:18 +0800

    fixes the wrong spelling

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next"
 
 const baseUrl =
-  process.env.NEXT_PUBLIC_APP_URL || "https://www.amoreframephotobooth.site"
+  process.env.NEXT_PUBLIC_SITE_URL || "https://www.amoreframephotobooth.site"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const currentDate = new Date()
